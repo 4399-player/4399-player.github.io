f9b25a69c3189952d33ae6de36364d01dff73764
4399-player <2509140187@qq.com>
2019-09-25 21:44:26 +0800

    update

--- js/myScript.js
+++ js/myScript.js
@@ -2,3 +2,15 @@ function myFunction(ID)
 {
 	window.alert("存档信息添加中，请稍等");
 }
+
+function zeroOne()
+{
+	var url = 'zero.html';
+	window.location.href = url;
+}
+
+function buyZeroOne()
+{
+	var url = 'https://w.url.cn/s/AVzevTK';
+	window.open(url);
+}
